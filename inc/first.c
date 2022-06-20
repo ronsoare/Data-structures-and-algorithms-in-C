0c33985411fccc923e2d5a507e02ca29e68efc71
nars95 <ronaldo.pmaa@hotmail.com>
2022-06-20 16:48:52 -0400

    add comment and a input to indicate the user how many student he would like read

--- inc/first.c
+++ inc/first.c
@@ -2,40 +2,48 @@
 #include<stdlib.h>
 #include<string.h>
 
+// struct that of datas
 typedef struct Data{
     char name[50];
     int id;
 } Data;
-typedef struct No{
+// struct of Node
+typedef struct Node{
     Data data;
-    struct No *next;
-} No;
+    struct Node *next;
+} Node;
+// struct for linked list
 typedef struct List{
-    No *first;
+    Node *first;
 } List;
+// function that inicialize a list;
 void inicialize( List *pont){
     pont -> first = NULL;
 }
+// funciont that use to implement a inserction in a linked list;
 void insert( List *pont, Data info){
-    No *aux = (No*) malloc(sizeof(No));
+    Node *aux = (Node*) malloc(sizeof(Node));
     aux -> data = info;
     aux -> next = pont -> first;
     pont -> first = aux;
 }
+// function that show each element ( student and id) of linked list
 void showList( List *pont){
-    No *aux = pont -> first;
+    Node *aux = pont -> first;
     while ( aux ){
         printf("name = %s\n", aux -> data.name);
         printf("id = %d\n", aux -> data.id);
         aux = aux -> next;
     }
 }
+// function that read information about student, such as your name and id.
 void readStudent( Data *data){
     printf("type the name: ");
     scanf("%[^\n]%*c", data -> name);
     printf("Type the id: ");
     scanf("%d%*c", &data -> id);
 }
+// fucntion that read a sequence of students.
 void readList( List *pont, Data *data, unsigned length){
      unsigned i  = 0;
      for ( i; i < length; i++){
@@ -43,18 +51,13 @@ void readList( List *pont, Data *data, unsigned length){
         insert(pont,*data);
      }
 }
-void showStudent( List *pont){
-    No *aux = pont -> first;
-    while ( aux ){
-        printf("name = %s\n", aux -> data.name);
-        printf("id = %d\n", aux -> data.id);
-        aux = aux -> next;
-    }
-}
 int main(){
     Data student;
     List list;
     inicialize(&list);
-    readList(&list, &student, 5);
+    int num;
+    printf("Hom many students would you like to read: ");
+    scanf("%d%*c",&num);
+    readList(&list, &student, num);
     showList(&list);
 }
